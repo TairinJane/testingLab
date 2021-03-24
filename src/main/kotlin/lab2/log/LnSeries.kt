77063f8a9c824713d19767078559933e50e23734
TairinJane <tairinjane@gmail.com>
2021-03-24 18:51:57 +0300

    fix: fix ln series

--- src/main/kotlin/lab2/log/LnSeries.kt
+++ src/main/kotlin/lab2/log/LnSeries.kt
@@ -10,12 +10,12 @@ open class Ln : Function {
 
 class LnSeries(private val precision: Double = 0.0001) : Ln() {
     private fun lnTerm(x: Double, n: Int): Double {
-        return (-1.0).pow(n) * (x - 1).pow(n + 1) / (n + 1)
+        return (-1.0).pow(n) * (x - 1).pow(n) / n
     }
 
     override fun value(x: Double): Double {
         var result = 0.0
-        var i = 0
+        var i = 1
         var term = lnTerm(x, i)
         do {
             result += term
