d41471f2169ca8b99777223daf13ed39ecc1c9c3
TairinJane <tairinjane@gmail.com>
2021-03-24 18:51:42 +0300

    feat: add numbers after comma param to csv writer

--- src/main/kotlin/lab2/csvWriter/CsvWriter.kt
+++ src/main/kotlin/lab2/csvWriter/CsvWriter.kt
@@ -3,11 +3,11 @@ package lab2.csvWriter
 import lab2.Function
 import java.io.File
 
-fun writeResultsToCsv(function: Function, from: Double, to: Double, step: Double, path: String = "results.csv") {
+fun writeResultsToCsv(function: Function, from: Double, to: Double, step: Double, path: String = "results.csv", nums: Int = 8) {
     File(path).printWriter().use { out ->
         var x = from
         while (x < to) {
-            out.println("%.8f, %.8f".format(x, function.value(x)))
+            out.println("%.${nums}f; %.${nums}f".format(x, function.value(x)))
             x += step
         }
     }
--- src/test/kotlin/lab2/FunctionSystemTest.kt
+++ src/test/kotlin/lab2/FunctionSystemTest.kt
@@ -10,7 +10,7 @@ class FunctionSystemTest {
 
     private val precision = 0.0001
 
-    private fun testValuesOnDifferentSteps(x: Double) {
+    private fun testValueOnDifferentSteps(x: Double) {
         val mockTypes = MockType.values()
         for (type in mockTypes) {
             val mock = getFunctionSystemMock(type)
@@ -26,24 +26,24 @@ class FunctionSystemTest {
     @ParameterizedTest(name = "Test log function at {0}")
     @ValueSource(doubles = [0.0, 0.76, 1.0, 1.365, 1.9974, 2.0, 6.12])
     fun `log function test`(x: Double) {
-        testValuesOnDifferentSteps(x)
+        testValueOnDifferentSteps(x)
     }
 
     @ParameterizedTest(name = "x != pi*N, x != pi*N - pi/2 at {0}")
     @ValueSource(doubles = [-PI * 3, -PI * 10 - PI / 2, -PI * 345, -PI * 22 - PI / 2])
     fun `cos function test`(x: Double) {
-        testValuesOnDifferentSteps(x)
+        testValueOnDifferentSteps(x)
     }
 
     @ParameterizedTest(name = "2(pi*N + 0.42773378844390394065) = 0, 2(pi*N + 1.1430625383509926786) = 0 at {0}")
     @ValueSource(doubles = [2*(PI * -5 + 0.42773378844390394065), 2*(PI * -63 + 1.1430625383509926786)])
     fun `f(x) = 0`(x: Double) {
-        testValuesOnDifferentSteps(x)
+        testValueOnDifferentSteps(x)
     }
 
     @ParameterizedTest(name = "stationary 2(pi*N - 1.18487), 2(pi*N - 0.385931) at {0}")
     @ValueSource(doubles = [2*(PI* -8 - 1.18487), 2*(PI* -43 - 0.385931)])
     fun stationary(x: Double) {
-        testValuesOnDifferentSteps(x)
+        testValueOnDifferentSteps(x)
     }
 }
