01e190f61e4df49870723f0f6092dddbe156010d
TairinJane <tairinjane@gmail.com>
2021-03-25 12:54:46 +0300

    test: more cases

--- src/test/kotlin/lab2/FunctionSystemTest.kt
+++ src/test/kotlin/lab2/FunctionSystemTest.kt
@@ -37,14 +37,38 @@ class FunctionSystemTest {
     }
 
     @ParameterizedTest(name = "2(pi*N + 0.42773378844390394065) = 0, 2(pi*N + 1.1430625383509926786) = 0 at {0}")
-    @ValueSource(doubles = [2*(PI * -5 + 0.42773378844390394065), 2*(PI * -63 + 1.1430625383509926786)])
+    @ValueSource(doubles = [2 * (PI * -5 + 0.42773378844390394065), 2 * (PI * -63 + 1.1430625383509926786)])
     fun `f(x) = 0`(x: Double) {
         testValueOnDifferentSteps(x)
     }
 
     @ParameterizedTest(name = "stationary 2(pi*N - 1.18487), 2(pi*N - 0.385931) at {0}")
-    @ValueSource(doubles = [2*(PI* -8 - 1.18487), 2*(PI* -43 - 0.385931)])
+    @ValueSource(doubles = [2 * (PI * -8 - 1.18487), 2 * (PI * -43 - 0.385931)])
     fun stationary(x: Double) {
+        testValueOnDifferentSteps(x)
+    }
+
+    @ParameterizedTest(name = "(2(PIn - 0.385931); 2PIn) at {0}")
+    @ValueSource(doubles = [-0.345, 2 * (PI * -9 - 0.2), 2 * (PI * -76 - 0.1082)])
+    fun cosDownCurveLower(x: Double) {
+        testValueOnDifferentSteps(x)
+    }
+
+    @ParameterizedTest(name = "(PI(n + 1/2); 2(PIn - 0.385931)) at {0}")
+    @ValueSource(doubles = [2 * (PI * -206 + 0.93), 2 * (PI * -9 + 0.7), 2 * (PI * -13 - 0.496)])
+    fun cosUpCurveLower(x: Double) {
+        testValueOnDifferentSteps(x)
+    }
+
+    @ParameterizedTest(name = "(PIn; 2(PIn - 1.18487)) at {0}")
+    @ValueSource(doubles = [-PI * 3 + 0.1, 2 * (PI * -7 - 1.6), 2 * (PI * -56 - 1.1982)])
+    fun cosDownCurveUpper(x: Double) {
+        testValueOnDifferentSteps(x)
+    }
+
+    @ParameterizedTest(name = "Long curve down at {0}")
+    @ValueSource(doubles = [2 * (PI * -142 + 0.3), PI * -16 + 0.35, 2 * (PI * -13 + 1.072), PI * -10 - 0.6])
+    fun cosUpCurveUpper(x: Double) {
         testValueOnDifferentSteps(x)
     }
 
@@ -61,7 +85,7 @@ class FunctionSystemTest {
     }
 
     @Test
-    fun `log at x=1`() {
+    fun `log at x = 1`() {
         testValueOnDifferentSteps(1.0)
     }
 }
