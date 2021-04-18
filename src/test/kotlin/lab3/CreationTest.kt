0dffa56a0e37736a47ca2254efb82d4d4b0e2e88
TairinJane <tairinjane@gmail.com>
2021-04-18 20:56:57 +0300

    test(lab3): new post

--- src/test/kotlin/lab3/CreationTest.kt
+++ src/test/kotlin/lab3/CreationTest.kt
@@ -1,6 +1,7 @@
 package lab3
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
@@ -25,26 +26,23 @@ class CreationTest {
 
     private fun login() {
         driver.run {
-            findElement(By.xpath("//a[contains(@class, 'sign-in')]")).click()
-            findElement(By.xpath("//input[@type='email']")).sendKeys("faketestse@gmail.com")
-            findElement(By.xpath("//button[span='Далее']")).click()
+            clickByXpath("//a[contains(@class, 'sign-in')]")
+            typeByXpath("//input[@type='email']", "faketestse@gmail.com")
+            clickByXpath("//button[span='Далее']")
             WebDriverWait(
                 driver,
                 Duration.ofSeconds(3).toSeconds()
             ).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='password']")))
                 .sendKeys("faketestsePass3,")
-            findElement(By.xpath("//button[span='Далее']")).click()
+            clickByXpath("//button[span='Далее']")
             if (isLoginNeeded()) {
-                WebDriverWait(
-                    driver,
-                    Duration.ofSeconds(3).toSeconds()
-                ).until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@class, 'sign-in')]"))).click()
-                findElement(By.xpath("//div[@data-identifier='faketestse@gmail.com']")).click()
+                waitForAndClick("//a[contains(@class, 'sign-in')]")
+                clickByXpath("//div[@data-identifier='faketestse@gmail.com']")
             }
         }
     }
 
-    fun createInitialBlog() {
+    private fun createInitialBlog() {
         driver.run {
             findElement(By.xpath("//input[@aria-label='Название']")).sendKeys("Tpo3")
             findElement(By.xpath("//div[@role='button'][span='Далее']")).click()
@@ -69,37 +67,53 @@ class CreationTest {
         }
     }
 
+    @BeforeEach
+    fun openBasePage() {
+        driver.get(baseUrl)
+        if (isLoginNeeded()) login()
+    }
+
     private fun addNewBlog() {
         driver.run {
             val randomNum = nextInt(50)
             get(baseUrl)
-            findElement(By.xpath("//div[@role='listbox'][@aria-label='Blog Selection']")).apply{
-                findElement(By.xpath("./div[@role='presentation']")).click()
-                findElement(By.xpath("./div[last()]/div[last()]")).click()
+            findElement(By.xpath("//div[@role='listbox'][@aria-label='Blog Selection']")).apply {
+                clickByXpath("./div[@role='presentation']")
+                clickByXpath("./div[last()]/div[last()]")
             }
-            findElement(By.xpath("//input[@aria-label='Название']")).sendKeys("New blog $randomNum")
-            findElement(By.xpath("//div[@role='button']/span[span='Далее']")).click()
-            findElement(By.xpath("//input[@aria-label='Адрес']")).sendKeys("seTpo$randomNum")
+            typeByXpath("//input[@aria-label='Название']", "New blog $randomNum")
+            clickByXpath("//div[@role='button']/span[span='Далее']")
+            typeByXpath("//input[@aria-label='Адрес']", "seTpo$randomNum")
             WebDriverWait(this, Duration.ofSeconds(3).toSeconds()).until(
                 ExpectedConditions.presenceOfElementLocated(
                     By.xpath("//div[.='Этот адрес блога не занят.']")
                 )
             )
-            WebDriverWait(
-                this,
-                Duration.ofSeconds(3).toSeconds()
-            ).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='button']/span[span='Сохранить']")))
-                .click()
+            waitForAndClick("//div[@role='button']/span[span='Сохранить']")
             Assertions.assertTrue(findElements(By.xpath("//div[@aria-label='Tpo$randomNum']")).isNotEmpty())
         }
     }
 
     @Test
-    fun createBlog() {
-        driver.get(baseUrl)
-        if (isLoginNeeded()) {
-            login()
-        }
+    fun createNewBlog() {
         addNewBlog()
+    }
+
+    @Test
+    fun addNewPost() {
+        driver.run {
+            clickByXpath("//div[@role='button'][@aria-label='Создать новое сообщение']")
+            findElement(By.xpath("//input[@aria-label='Название']")).apply {
+                click()
+                sendKeys("New post")
+            }
+            switchTo().frame(findElement(By.xpath("//iframe[contains(@class, 'editable')]")))
+            typeByXpath("//body", "Post body")
+            switchTo().defaultContent()
+            clickByXpath("//span[@title='Изменения сохранены.']")
+            waitForAndClick("//div[@role='button'][@aria-label='Опубликовать'][@data-tooltip='Опубликовать']/*//div[.='Опубликовать']", 5)
+            waitForAndClick("//span[.='При этом сообщение будет опубликовано в вашем блоге.']/following-sibling::div/div[@role='button']/span/span[.='ОК']")
+            Assertions.assertTrue(findElements(By.xpath("//div[@role='listitem']//span[.='New post']")).isNotEmpty())
+        }
     }
 }
---
+++ src/test/kotlin/lab3/Utils.kt
@@ -0,0 +1,23 @@
+package lab3
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.time.Duration
+
+fun WebDriver.clickByXpath(xpath: String) {
+    findElement(By.xpath(xpath)).click()
+}
+
+fun WebDriver.typeByXpath(xpath: String, text: String) {
+    findElement(By.xpath(xpath)).sendKeys(text)
+}
+
+fun WebDriver.waitForAndClick(xpath: String, seconds: Long = 3) {
+    WebDriverWait(
+        this,
+        Duration.ofSeconds(seconds).toSeconds()
+    ).until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)))
+        .click()
+}
